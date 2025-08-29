6c62f825e7a0a72d53545076695d0b46a152a546
zalvarado1 <Zayrahw28@icloud.com>
2025-08-29 14:31:12 -0700

    Worked on Assignment 0.1

---
+++ Assignments/Unit 0 - Conditional Challenge/conditionalhallenge.java
@@ -0,0 +1,39 @@
+public class conditionalhallenge {
+    public static void main(String[] args) {
+        // Your code will go here
+        double Temperature = 20;
+        System.out.println("Initial Temperature: " + Temperature);
+        boolean isCold = true;
+        
+
+        
+        // Part 1: Temperature Converter
+        System.out.println("--- Part 1: Temperature Converter ---");
+        if (isCold = true) {
+            System.out.println("isCold" + isCold);
+            double NewTemperature = 5;
+            System.out.println("New Temperature: " + NewTemperature);
+        }
+        // Part 2: Grade Calculator
+        System.out.println("--- Part 2: Grade ---");
+        int Score = 90;
+        System.out.println("Score: " + Score);
+        if (Score >= 70); {
+        System.out.println("Passed");
+        }
+
+        
+        // Part 3
+        int SeasonTemperature = 45;
+        if (SeasonTemperature < 59 || SeasonTemperature > 40); {
+            System.out.println("Fall.");
+        }
+
+
+        
+        // Part 4
+        int Number = 34;
+        if (Number > 0 || Number < 100)
+        System.out.println("Positive.");
+    }
+}
